e1fcb57f2427d5a3e00bba708f9bab5292749076
Reck11 <118613899+Reck11@users.noreply.github.com>
2024-01-29 17:10:16 +0100

    model pickled

--- src/stock/main.py
+++ src/stock/main.py
@@ -1,31 +1,59 @@
-import yfinance as yf
-from pandas_datareader import data as pdr
-import pandas as pd
-import zipfile
 import os
+import sys
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
 
-with open('symbols.txt', 'r') as file:
-    data = file.read()
-symbols = data.split()
+def preprocess_data(data_folder):
 
-print(symbols)
-print(len(symbols))
+    dfs = []
+    all_data = pd.DataFrame()
 
-yf.pdr_override()
-stocks = [symbols]
+    for file_name in os.listdir(data_folder):
+        if file_name.endswith("_data.csv"):
+            file_path = os.path.join(data_folder, file_name)
+            stock_data = pd.read_csv(file_path)
+            dfs.append(stock_data)
 
-all_data = pd.DataFrame()
+    all_data = pd.concat(dfs, ignore_index=True)
 
-for stock in symbols:
-    data = pdr.get_data_yahoo(stock, start="2019-01-01", end="2024-01-15")
-    data['Symbol'] = stock
+    all_data['Up'] = (all_data['Close'] - all_data['Open'] > 0).astype(int)
 
-    data.to_csv(f'{stock}_data.csv')
+    features = all_data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
 
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
 
-with zipfile.ZipFile('stocks_data.zip', 'w') as zipf:
-    for stock in symbols:
-        zipf.write(f'{stock}_data.csv')
+    labels = all_data['Up']
 
-for stock in symbols:
-    os.remove(f'{stock}_data.csv')
+    X_train, X_test, y_train, y_test = train_test_split(features_scaled, labels, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+def build_model(X_train, X_test, y_train, y_test):
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Accuracy: {accuracy}")
+
+    conf_matrix = confusion_matrix(y_test, predictions)
+    print(f"Confusion Matrix:\n{conf_matrix}")
+
+    return model
+
+project_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+data_folder = project_dir + '/data'
+
+X_train, X_test, y_train, y_test = preprocess_data(data_folder)
+
+model = build_model(X_train, X_test, y_train, y_test)
+
+with open(project_dir + '/model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
